d1a3e894be2df9522dae5c189b2a26e6ee16fa5b
Manan Anand <manananand11@gmail.com>
2021-12-25 01:56:25 +0530

    final

--- src/components/Collection.jsx
+++ src/components/Collection.jsx
@@ -3,6 +3,11 @@ import { Button, TextField, Typography } from '@mui/material'
 import { makeStyles } from '@mui/styles'
 import React from 'react'
 import Card3 from './Card3'
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
 
 const useStyles = makeStyles({
     space: {
@@ -48,15 +53,24 @@ const useStyles = makeStyles({
         marginTop: "50px",
         justifyContent: "space-between"
     },
-    buttonsmall:{
+    buttonsmall: {
         color: "grey!important",
         fontWeight: "500!important",
-        fontSize:"12px!important"
+        fontSize: "12px!important"
     }
 
 })
 
 export default function Collection() {
+    const [open, setOpen] = React.useState(false);
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
     const classes = useStyles()
     return (
         <div>
@@ -101,8 +115,56 @@ export default function Collection() {
                 Text that will be used in VoiceOver for people with disabilities
             </Typography>
             <div className={classes.unlockdiv}>
-                <Button className={classes.buttongrad} variant="outlined">Create Item</Button>
-                <Button className={classes.buttonsmall} size="small">Unsaved Changes <HelpOutlineOutlined/></Button>
+                <Button onClick={handleClickOpen} className={classes.buttongrad} variant="outlined">Create Item</Button>
+                <Button className={classes.buttonsmall} size="small">Unsaved Changes <HelpOutlineOutlined /></Button>
+            </div>
+            <div>
+
+                <Dialog
+                    open={open}
+                    onClose={handleClose}
+                    aria-labelledby="alert-dialog-title"
+                    aria-describedby="alert-dialog-description"
+                >
+                    <DialogTitle id="alert-dialog-title">
+                        {"Collection"}
+                    </DialogTitle>
+                    <DialogContent>
+                        <Typography variant="subtitle2" className={classes.space}>
+                            Display Name <span className={classes.text}>(required)</span>
+                        </Typography>
+
+                        <TextField fullWidth id="standard-basic" placeholder="Enter Token NAME" variant="standard" />
+                        <Typography style={{ display: 'block' }} variant="caption" className={classes.text}>
+                            Will be used as public URL
+                        </Typography>
+                        <Typography variant="subtitle2" className={classes.space}>
+                            Symbol <span className={classes.text}>(optional)</span>
+                        </Typography>
+
+                        <TextField fullWidth id="standard-basic" placeholder="Enter Token Symbol" variant="standard" />
+                        <Typography variant="subtitle2" className={classes.space}>
+                            Description <span className={classes.text}>(optional)</span>
+                        </Typography>
+
+                        <TextField fullWidth id="standard-basic" placeholder="Spread some word about your token collection" variant="standard" />
+                        <Typography variant="subtitle2" className={classes.space}>
+                            Short URL <span className={classes.text}>(optional)</span>
+                        </Typography>
+
+                        <TextField fullWidth id="standard-basic" placeholder="fungy.com/ Enter Short URL" variant="standard" />
+                        <Typography style={{ display: 'block' }} variant="caption" className={classes.text}>
+                            Will be used as public URL
+                        </Typography>
+                        <DialogContentText id="alert-dialog-description">
+                            Let Google help apps determine location. This means sending anonymous
+                            location data to Google, even when no apps are running.
+                        </DialogContentText>
+                    </DialogContent>
+                   <div className={classes.center}>
+                    <Button className={classes.buttongrad} variant="outlined">Create Collection</Button>
+                    </div>
+                </Dialog>
             </div>
         </div>
     )
